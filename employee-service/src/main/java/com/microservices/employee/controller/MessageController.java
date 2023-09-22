684a096e5c12842ad35bc7ebe35075eb529207e1
Gyarababu <gyarababu9@gmail.com>
2023-09-22 18:00:33 +0530

    V2.0

--- employee-service/src/main/java/com/microservices/employee/controller/MessageController.java
+++ employee-service/src/main/java/com/microservices/employee/controller/MessageController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RefreshScope
 @RestController
 public class MessageController {
