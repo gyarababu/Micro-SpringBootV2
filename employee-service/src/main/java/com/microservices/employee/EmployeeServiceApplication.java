38205ba40cc0685b8dd1cda5799c500e3371d5b7
Gyarababu <gyarababu9@gmail.com>
2023-09-22 18:32:37 +0530

    downgraded to springboot v2.7.15 for using features which only support v2

--- employee-service/src/main/java/com/microservices/employee/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/microservices/employee/EmployeeServiceApplication.java
@@ -3,12 +3,14 @@ package com.microservices.employee;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	@Bean
